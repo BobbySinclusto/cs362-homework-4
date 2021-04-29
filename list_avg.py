110cf137fbe79095412062f6a6f0fe955e053a97
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-28 23:28:30 -0700

    added another edge case

--- list_avg.py
+++ list_avg.py
@@ -3,6 +3,10 @@ def list_avg(arr):
         if len(arr) > 0:
             total = 0
             for thing in arr:
+                if type(thing) == int or type(thing) == float:
+                    total += thing
+                else:
+                    raise TypeError('List elements must be numbers.')
             return total / len(arr)
         else:
             raise ValueError('List must have at least one element.')
