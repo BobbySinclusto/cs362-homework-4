c2b05134713cd98a02389ab1001b89ce46584502
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-28 23:37:17 -0700

    added list average calculator tester

---
+++ test_list_avg.py
@@ -0,0 +1,20 @@
+import unittest
+import list_avg
+
+class testListAvg(unittest.TestCase):
+    def test_list_avg_good(self):
+        self.assertEqual(list_avg.list_avg([3, 4, 5, 6]), 4.5)
+        self.assertEqual(list_avg.list_avg([-3.5,3.5]), 0)
+    
+    def test_list_avg_fail(self):
+        self.assertEqual(list_avg.list_avg([3,3,3]), 1)
+    
+    def test_list_avg_empty(self):
+        self.assertRaises(ValueError, list_avg.list_avg, [])
+    
+    def test_list_avg_negative(self):
+        self.assertRaises(TypeError, list_avg.list_avg, [3, 4, 'asdf'])
+        self.assertRaises(TypeError, list_avg.list_avg, ['203', '101', '3'])
+
+if __name__ == '__main__':
+    unittest.main()
